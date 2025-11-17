5132a738ebd8769fb175caf540a38841d28e660e
kwonseokki <kwons4163@naver.com>
2025-11-17 18:49:43 +0900

    Time: 2 ms (27.43%), Space: 12.8 MB (25.71%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -2,8 +2,10 @@ class Solution(object):
     def floodFill(self, image, sr, sc, color):
         n = len(image)
         m = len(image[0])
-        visited = [[False for _ in range(m)] for _ in range(n)]
         sp = image[sr][sc]
+
+        if color == sp:
+            return image
 
         def fill(sr, sc):
             if sr < 0 or sr >= n or sc < 0 or sc >= m:
@@ -13,10 +15,6 @@ class Solution(object):
                 image[sr][sc] = color
             else:
                 return
-            # 이미 방문한경우 함수 종료
-            if visited[sr][sc]:
-                return
-            visited[sr][sc] = True
 
             fill(sr+1, sc)
             fill(sr-1, sc)
